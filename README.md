3af01e92cde7b3ffd693035202602a4be0715979
Bingo <Bentleyjobs@gmail.com>
2018-05-04 01:08:48 +0800

    added some indispensable lib
    1.gui cam control supported

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SKTWeb
-A painter util demo based on WebGL/[Three.js](https://threejs.org/). 
+A painter util demo based on [Three.js](https://threejs.org/)(WebGL). 
 
 ## Debug
 Using [Browsersync](browsersync.io) for convient developing.
@@ -11,4 +11,6 @@ browser-sync start --server --files "**"
 > The code above would automatically update the web page if comes any changes.
 
 ## Dependency
-1. [Three.js](https://threejs.org/) for WebGL packaging.(Contained)
+1. [Three.js](https://threejs.org/) for WebGL packaging.
+2. [dat.GUI](https://github.com/dataarts/dat.gui) for supporting GUI on canvas.
+3. [OrbitControls](https://threejs.org/docs/#examples/controls/OrbitControls) allow the camera to orbit around a target.
--- js/scene.js
+++ js/scene.js
@@ -1,24 +1,82 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// local var
+var renderer;
+var camera, controls;
+var scene, cube;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// GUI
+function Demo() {
+  this.Demo1 = 'SKT';
+  this.Demo2 = 0.8;
+  this.Demo3 = false;
+  this.Demo4 = function() {};
+};
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function initGUI() {
+  	var text = new Demo();
+    var gui = new dat.GUI();
+	gui.add(text, 'Demo1');
+	gui.add(text, 'Demo2', -5, 5);
+	gui.add(text, 'Demo3');
+	gui.add(text, 'Demo4');
+}
 
-camera.position.z = 5;
+// Scene
+function init() {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-var animate = function() {
-    requestAnimationFrame(animate);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // Camera drag event support
+	controls = new THREE.OrbitControls(camera, renderer.domElement );
+	controls.addEventListener('change', render);
 
+    // Objects
+    var geometry = new THREE.SphereBufferGeometry( 1, 32, 32 );
+    var material = new THREE.MeshPhongMaterial( { color: 0xffffff, dithering: true } );
+    object = new THREE.Mesh(geometry, material);
+    scene.add(object);
+
+    // Lighting
+	var ambient = new THREE.AmbientLight( 0xffffff, 0.1 );
+	scene.add( ambient );
+
+	spotLight = new THREE.SpotLight( 0xffffff, 1 );
+	spotLight.position.set( 15, 40, 35 );
+	spotLight.angle = Math.PI / 4;
+	spotLight.penumbra = 0.05;
+	spotLight.decay = 2;
+	spotLight.distance = 200;
+	spotLight.castShadow = true;
+	spotLight.shadow.mapSize.width = 1024;
+	spotLight.shadow.mapSize.height = 1024;
+	spotLight.shadow.camera.near = 10;
+	spotLight.shadow.camera.far = 200;
+	scene.add( spotLight );
+
+	// GUI
+    initGUI();
+
+    // resize the canvas when window changed
+    window.addEventListener('resize', resize, false);
+}
+
+function render() {
+    requestAnimationFrame(render);
     renderer.render(scene, camera);
 };
 
-animate();
+function resize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// frame loop
+init();
+render();
+    
